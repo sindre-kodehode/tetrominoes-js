2dfb199a0c136baeb5733ab8755bccef1a087923
sindre-kodehode <sindre.kodehode@gmail.com>
2022-09-28 14:12:14 +0200

    TET-4: add gamepad support

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
+import Buffer     from "./Buffer.js";
+import Piece      from "./Piece.js";
+import Playfield  from "./Playfield.js";
+import Controller from "./Controller.js";
+
 import { 
   HEIGHT,  
   MILLI,
   WIDTH, 
 } from "./consts.js";
 
-import Buffer    from "./Buffer.js";
-import Piece     from "./Piece.js";
-import Playfield from "./Playfield.js";
 
 const cells = Array( HEIGHT * WIDTH ).fill().map( () =>
     document.createElement( "td" ) );
@@ -22,18 +24,30 @@ const table = document.createElement( "table" );
 table.append( ...rows );
 document.body.append( table );
 
-const playfield = new Playfield();
-const piece     = new Piece( playfield );
-const buffer    = new Buffer( piece, playfield, cells );
+const playfield  = new Playfield();
+const piece      = new Piece( playfield );
+const buffer     = new Buffer( piece, playfield, cells );
+const controller = new Controller( piece );
 
 setInterval( () => {
   buffer.render();
+  controller.update();
+  controller.buttonPress();
 }, MILLI );
 
 document.addEventListener( "keydown", ({ key }) => {
   switch( key ) {
-    case "ArrowLeft"  : piece.moveLeft()  ; break ;
-    case "ArrowRight" : piece.moveRight() ; break ;
-    case "ArrowDown"  : piece.moveDown()  ; break ;
-    case "ArrowUp"    : piece.rotate()    ; break ;
+    case "ArrowLeft"  : piece.moveLeft()         ; break ;
+    case "ArrowRight" : piece.moveRight()        ; break ;
+    case "ArrowDown"  : piece.moveDown()         ; break ;
+    case "ArrowUp"    : piece.rotate()           ; break ;
+    case "d"          : controller.toggleDebug() ; break ;
 }});
+
+window.addEventListener( "gamepadconnected", e => {
+  controller.connect( e );
+});
+
+window.addEventListener( "gamepaddisconnected", e => {
+  controller.disconnect( e );
+});
